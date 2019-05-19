c935cfdab97c611a2ff0fe0a55c93136584620ce
Sean Conrad <41274343+sconrad10@users.noreply.github.com>
2019-05-19 00:21:19 -0400

    Add files via upload

---
+++ TicTacToeConstants.java
@@ -0,0 +1,26 @@
+// ==================================
+// Project: TicTacToe
+// Programmer: Sean Conrad
+// ==================================
+
+package com.sconrad;
+
+
+public interface TicTacToeConstants {
+    int PLAYER1 = 1; // Indicate player 1
+
+
+    int PLAYER2 = 2; // Indicate player 2
+
+
+    int PLAYER1_WON = 1; // Indicate player 1 won
+
+
+    int PLAYER2_WON = 2; // Indicate player 2 won
+
+
+    int DRAW = 3; // Indicate a draw
+
+
+    int CONTINUE = 4; // Indicate to continue
+}
